6117e40965baf929a549e5297b8fdef3e2c2e7bd
MoonSsunny <moonsun116@naver.com>
2020-08-08 16:43:47 +0900

    좌표찾기 finished

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const targetImg = document.querySelector(".target_img");
 const log = document.querySelector("#log");
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
 
 function resultFunction(e) {
     let positionLeft = e.clientX;
     let positionTop = e.clientY;
 
-    console.log(positionTop);
-    console.log(positionLeft);
-
     targetImg.style.left = positionLeft + "px";
     targetImg.style.top = positionTop + "px";
+    vertical.style.left = positionLeft + "px";
+    horizontal.style.top = positionTop + "px";
 
-    log.innerText = `${e.clientX}, ${e.clientY}`;
+    log.innerText = `${e.clientX}px, ${e.clientY}px`;
 }
 
 document.addEventListener("mousemove", resultFunction);
