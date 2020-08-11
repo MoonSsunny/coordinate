5b4b86e7d3a2592a179143886e263dcc418097ad
MoonSsunny <moonsun116@naver.com>
2020-08-11 19:54:38 +0900

    css 성능개선

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ const log = document.querySelector("#log");
 const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 
-function resultFunction(e) {
-    let positionLeft = e.clientX;
-    let positionTop = e.clientY;
+addEventListener("load", () => {
+    const targetRect = targetImg.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHight = targetRect.height / 2;
 
-    targetImg.style.left = positionLeft + "px";
-    targetImg.style.top = positionTop + "px";
-    vertical.style.left = positionLeft + "px";
-    horizontal.style.top = positionTop + "px";
+    function resultFunction(e) {
+        let positionLeft = e.clientX;
+        let positionTop = e.clientY;
 
-    log.innerText = `${e.clientX}px, ${e.clientY}px`;
-}
+        vertical.style.transform = `translateX(${positionLeft}px)`;
+        horizontal.style.transform = `translateY(${positionTop}px)`;
+        targetImg.style.transform = `translate(${
+            positionLeft - targetHalfWidth
+        }px, ${positionTop - targetHalfHight}px)`;
 
-document.addEventListener("mousemove", resultFunction);
+        log.innerText = `${e.clientX}px, ${e.clientY}px`;
+    }
+
+    document.addEventListener("mousemove", resultFunction);
+});
